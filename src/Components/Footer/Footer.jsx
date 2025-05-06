64ca8cdf5c0ae7ba12d84c1f5c2e7ef7fd502258
Sivapragasam <qc.siva@gmail.com>
2025-05-06 21:00:16 +0530

    All reworks done

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Footer.css'
-import footer_logo from '../../assets/footer_logo.svg'
+import footer_logo from '../../assets/logo1.svg'
 import user_icon from '../../assets/user_icon.svg'
 
 const Footer = () => {
@@ -8,7 +8,7 @@ const Footer = () => {
         <div className='footer' >
             <div className="footer-top">
                 <div className="footer-top-left">
-                    <img src={footer_logo} alt="" />
+                    <img src={footer_logo} alt="" style={{ width: "100px", height: "100px" }} />
                     <p>I am a Full stack developer and also worked Non-IT filed more than 10 years</p>
                 </div>
                 <div className="footer-top-right">
--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,40 +1,46 @@
-import project1_img from "../assets/project_1.svg";
-import project2_img from "../assets/project_2.svg";
-import project3_img from "../assets/project_3.svg";
-import project4_img from "../assets/project_4.svg";
-import project5_img from "../assets/project_5.svg";
-import project6_img from "../assets/JobPortal_image.png";
+import project1_img from "../assets/JobPortal_image.png";
+import project2_img from "../assets/My_portfolio.png";
+import project3_img from "../assets/Books_library.png";
+import project4_img from "../assets/Cart_page.png";
+import project5_img from "../assets/Todo_list.png";
+import project6_img from "../assets/Address_list.png";
 
 const mywork_data = [
   {
     w_no: 1,
     w_name: "Web design",
     w_img: project1_img,
+    w_link: "https://incredible-boba-b3094a.netlify.app/jobspage",
   },
   {
     w_no: 2,
     w_name: "Web design",
     w_img: project2_img,
+    w_link: "https://idyllic-unicorn-7f1f2a.netlify.app/",
   },
   {
     w_no: 3,
     w_name: "Web design",
     w_img: project3_img,
+    w_link: "https://steady-donut-4ab939.netlify.app/",
   },
   {
     w_no: 4,
     w_name: "Web design",
     w_img: project4_img,
+    w_link: "https://funny-sprinkles-a40028.netlify.app/",
   },
   {
     w_no: 5,
     w_name: "Web design",
     w_img: project5_img,
+    w_link: "https://majestic-meerkat-9edec6.netlify.app/",
   },
   {
     w_no: 6,
     w_name: "Web design",
     w_img: project6_img,
+    w_link: "https://heartfelt-madeleine-a54bf6.netlify.app/",
   },
 ];
 
