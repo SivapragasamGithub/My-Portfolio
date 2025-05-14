084d6cf15b93e1d02bc0a717ee442a91bacb85c5
Sivapragasam <qc.siva@gmail.com>
2025-05-14 23:49:21 +0530

    Resume Added

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import './Hero.css'
 import profile_img from '../../assets/siva-face.svg'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+import Non_IT from '../../assets/Resume-NonIT.pdf'
+import IT from '../../assets/IT.pdf'
+
 
 const Hero = () => {
     return (
@@ -11,7 +14,8 @@ const Hero = () => {
             <p>I am Sivapragasam Full stack developer, Having strong Knowledge in Full stack with live projects</p>
             <div className="hero-action">
                 <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>  Connect with me </AnchorLink> </div>
-                <div className="hero-resume">My Resume</div>
+                <a className='Non-IT' href={Non_IT} target="_blank" rel="noopener noreferrer">Non-IT resume</a>
+                <a className='hero-resume' href={IT} target="_blank" rel="noopener noreferrer">IT resume</a>
             </div>
         </div>
     )
